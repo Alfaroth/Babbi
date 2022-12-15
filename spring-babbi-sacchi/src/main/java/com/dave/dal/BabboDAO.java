dfeffda8ce1d163c18b16683d6ac770440c48e9a
Alfaroth <114079014+Alfaroth@users.noreply.github.com>
2022-12-15 10:47:43 +0100

    Delete spring-babbi-sacchi directory

--- spring-babbi-sacchi/src/main/java/com/dave/dal/BabboDAO.java
+++
@@ -1,9 +0,0 @@
-package com.dave.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dave.entities.Babbo;
-
-public interface BabboDAO extends JpaRepository<Babbo, Integer> {
-
-}
--- spring-babbi-sacchi/src/main/java/com/dave/dal/SaccoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.dave.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dave.entities.Sacco;
-
-public interface SaccoDAO extends JpaRepository<Sacco, Integer> {
-
-}
